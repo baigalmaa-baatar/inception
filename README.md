06d71f40bebefc915ee32124f95b3ff30787da7a
stardustd <baigalma@gmail.com>
2022-09-27 16:59:33 +0200

    hi

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ inception
 
 ## NGINX + HTTPS 101: The Basics & Getting Started:
 https://www.nginx.com/blog/nginx-https-101-ssl-basics-getting-started/
+
+-The evaluated student must be able to explain you how to login into
+the database. Try to login into the SQL database as root but with no
+password. If the login is successful, the evaluation process ends now.
+- Try to login into the SQL database with the user account and its
+password. Verify that the database is not empty.
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -5,12 +5,12 @@ set -m
 
 while ! nc -z mariadb 3306 ; do sleep 1 ; done
 
-wp config create --allow-root --dbname=wpdb --dbuser=root --dbpass=aqwe123 --dbhost=mariadb || true
+wp config create --allow-root --dbname="$DB_NAME" --dbuser="$WP_DB_ADMIN" --dbpass="$DB_PASS" --dbhost="$WP_DB_HOST" || true
 
-wp core install --allow-root --url=localhost --title=inception --admin_user=root --admin_email=inception@gmail.com || true
+wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_DB_ADMIN" --admin_email="$WP_ADM_MAIL" || true
 
-wp user create bbaatar bbaatar@example.com --role=administrator --user_pass=pass1 --allow-root || true
+wp user create "$WP_ADMIN" "$WP_ADMIN_MAIL" --role=administrator --user_pass="$WP_ADMIN_PASS" --allow-root || true
 
-wp user create bob1 bob1@example.com --role=author --user_pass=pass --allow-root || true
+wp user create "$WP_USER" "$WP_USER_MAIL" --role=author --user_pass="$WP_USER_PASS" --allow-root || true
 
 fg
