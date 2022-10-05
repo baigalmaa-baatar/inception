0cd47f175d2882ff3438757e18c7bcfe87b8e3d0
Baigalmaa BAATAR <bbaatar@e2r5p2.clusters.42paris.fr>
2022-10-05 11:26:59 +0200

    changed in Linux ver

--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# # terminate on errors
-# set -e
-
-# # Check if volume is empty
-# if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
-#     echo 'Setting up wp-content volume'
-#     # Copy wp-content from Wordpress src to volume
-#     cp -r /usr/src/wordpress/wp-content /var/www/
-#     chown -R nobody.nobody /var/www
-
-#     # Generate secrets
-#     curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
-# fi
-
 while ! nc -z mariadb 3306 ; do sleep 1 ; done
 
 cd /var/www/html/
